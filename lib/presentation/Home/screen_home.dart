5c75d1aa669241eb595c3b608be76b10ebed0c69
Akhil-ps-dev <psakhil211@gmail.com>
2022-10-17 20:32:33 +0530

    scroll-direction completed

--- lib/presentation/Home/screen_home.dart
+++ lib/presentation/Home/screen_home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_movie_app/presentation/Home/widget/background_image.dart';
 import 'package:flutter_movie_app/presentation/Home/widget/number_tile_card.dart';
-
 import '../../core/const.dart';
 import 'widget/main_title_card.dart';
 
@@ -31,6 +31,7 @@ class ScreenHome extends StatelessWidget {
               children: [
                 ListView(
                   children: const [
+                    CustomBackgroundImage(),
                     MainTitleCard(
                       title: 'Released in Past year',
                     ),
@@ -54,10 +55,59 @@ class ScreenHome extends StatelessWidget {
                   ],
                 ),
                 scrollDirectionVisible.value == true
-                    ? Container(
+                    ? AnimatedContainer(
+                        duration: const Duration(milliseconds: 1000),
                         width: double.infinity,
-                        height: 60,
-                        color: Colors.red,
+                        height: 90,
+                        color: Colors.black.withOpacity(0.3),
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                Image.network(
+                                  'https://cdn-images-1.medium.com/max/1200/1*ty4NvNrGg4ReETxqU2N3Og.png',
+                                  width: 60,
+                                  height: 60,
+                                ),
+                                const Spacer(),
+                                const Icon(
+                                  Icons.cast,
+                                  color: Colors.white,
+                                  size: 30,
+                                ),
+                                kWidth,
+                                Container(
+                                  width: 30,
+                                  height: 30,
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(3),
+                                      image: const DecorationImage(
+                                          image: NetworkImage(
+                                              "https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"),
+                                          fit: BoxFit.cover)),
+                                ),
+                                kWidth
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Text(
+                                  'Tv Shows',
+                                  style: kHomeTileText,
+                                ),
+                                Text(
+                                  'Movies',
+                                  style: kHomeTileText,
+                                ),
+                                Text(
+                                  'Categories',
+                                  style: kHomeTileText,
+                                )
+                              ],
+                            )
+                          ],
+                        ),
                       )
                     : kHeight
               ],
