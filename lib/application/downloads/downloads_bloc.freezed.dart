1f7a7f337398d5f64d4133706837d5037122392b
Akhil-ps-dev <psakhil211@gmail.com>
2022-10-20 21:43:25 +0530

    added-freezed

---
+++ lib/application/downloads/downloads_bloc.freezed.dart
@@ -0,0 +1,165 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+
+part of 'downloads_bloc.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$DownloadsEvent {
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() getDownloadsImage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? getDownloadsImage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? getDownloadsImage,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_GetDownloadsImage value) getDownloadsImage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_GetDownloadsImage value)? getDownloadsImage,
+  }) =>
+      throw _privateConstructorUsedError;
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_GetDownloadsImage value)? getDownloadsImage,
+    required TResult orElse(),
+  }) =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $DownloadsEventCopyWith<$Res> {
+  factory $DownloadsEventCopyWith(
+          DownloadsEvent value, $Res Function(DownloadsEvent) then) =
+      _$DownloadsEventCopyWithImpl<$Res, DownloadsEvent>;
+}
+
+/// @nodoc
+class _$DownloadsEventCopyWithImpl<$Res, $Val extends DownloadsEvent>
+    implements $DownloadsEventCopyWith<$Res> {
+  _$DownloadsEventCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+}
+
+/// @nodoc
+abstract class _$$_GetDownloadsImageCopyWith<$Res> {
+  factory _$$_GetDownloadsImageCopyWith(_$_GetDownloadsImage value,
+          $Res Function(_$_GetDownloadsImage) then) =
+      __$$_GetDownloadsImageCopyWithImpl<$Res>;
+}
+
+/// @nodoc
+class __$$_GetDownloadsImageCopyWithImpl<$Res>
+    extends _$DownloadsEventCopyWithImpl<$Res, _$_GetDownloadsImage>
+    implements _$$_GetDownloadsImageCopyWith<$Res> {
+  __$$_GetDownloadsImageCopyWithImpl(
+      _$_GetDownloadsImage _value, $Res Function(_$_GetDownloadsImage) _then)
+      : super(_value, _then);
+}
+
+/// @nodoc
+
+class _$_GetDownloadsImage implements _GetDownloadsImage {
+  const _$_GetDownloadsImage();
+
+  @override
+  String toString() {
+    return 'DownloadsEvent.getDownloadsImage()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType && other is _$_GetDownloadsImage);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  TResult when<TResult extends Object?>({
+    required TResult Function() getDownloadsImage,
+  }) {
+    return getDownloadsImage();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? whenOrNull<TResult extends Object?>({
+    TResult? Function()? getDownloadsImage,
+  }) {
+    return getDownloadsImage?.call();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeWhen<TResult extends Object?>({
+    TResult Function()? getDownloadsImage,
+    required TResult orElse(),
+  }) {
+    if (getDownloadsImage != null) {
+      return getDownloadsImage();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult map<TResult extends Object?>({
+    required TResult Function(_GetDownloadsImage value) getDownloadsImage,
+  }) {
+    return getDownloadsImage(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult? mapOrNull<TResult extends Object?>({
+    TResult? Function(_GetDownloadsImage value)? getDownloadsImage,
+  }) {
+    return getDownloadsImage?.call(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  TResult maybeMap<TResult extends Object?>({
+    TResult Function(_GetDownloadsImage value)? getDownloadsImage,
+    required TResult orElse(),
+  }) {
+    if (getDownloadsImage != null) {
+      return getDownloadsImage(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _GetDownloadsImage implements DownloadsEvent {
+  const factory _GetDownloadsImage() = _$_GetDownloadsImage;
+}
---
+++ lib/application/downloads/downloads_state.dart
@@ -0,0 +1,6 @@
+part of 'downloads_bloc.dart';
+
+@immutable
+abstract class DownloadsState {}
+
+class DownloadsInitial extends DownloadsState {}
