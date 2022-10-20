b9a2fd809cfea3fd902ec7731fcdc7b126219593
Akhil-ps-dev <psakhil211@gmail.com>
2022-10-20 17:41:12 +0530

    created models-freezed

--- lib/domain/core/api_end_points.dart
+++ lib/domain/core/api_end_points.dart
@@ -1,6 +1,11 @@
 import 'package:flutter_movie_app/core/strings.dart';
 import 'package:flutter_movie_app/infrastructure/api_key.dart';
 
+
+
+//path(base_url)
+//lib\core\strings.dart
+
 class ApiEndPoints {
   static const downloads = "$kBaseUrl/trending/all/day?api_key=$apiKey";
 }
---
+++ lib/domain/downloads/models/downloads.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'downloads.g.dart';
+ part 'downloads.freezed.dart';
+//pts
+
+@freezed
+class Downloads with _$Downloads {
+  const factory Downloads({
+    @JsonKey(name: "poster_path") required String? posterPath,
+  }) = _Downloads;
+
+  factory Downloads.fromJson(Map<String, dynamic> json) =>
+      _$DownloadsFromJson(json);
+}
